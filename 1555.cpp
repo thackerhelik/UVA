b695f0f4abd0bb6e1f2b5c2ad0d6f2cf32fbf408
thackerhelik <thackerhelik@users.noreply.github.com>
2018-09-22 06:53:41 +0530

    Update 1555.cpp

--- 1555.cpp
+++ 1555.cpp
@@ -2,7 +2,7 @@
 Substitute H1 in H2 formula then H2 in H3 forumula and so on until Hn-1 ... (we can generalize after 2-3 values)
 Formula comes out to be
 (n-1)(n-2)...(n-k)Hn-k = k(n-2)(n-3)...(n-k)A + (n-2)(n-3)...(n-k-1)B - k(n-1)(n-2)...(n-k-1)
-Hn-k should be positive for all k so find the corresponding B.
+Hn-k should be positive for all k from 1 to n - 2 so find the corresponding B. k = 1 corresponds to Hn-1 and k = n - 1 corresponds to H1
 B >= (k(n-1)(n-k-1) - ka) / (n-k-1)
 */
 #include <iostream>
@@ -19,7 +19,7 @@ int main(){
     while(cin >> n){
     	cin >> a;
     	ans = 0.0;
-    	for(k = 1; k < n; ++k){
+    	for(k = 1; k < (n-1); ++k){
     		val = (1.0*k*(n-1)*(n-k-1) - k*1.0*a)/(n-k-1);
     		ans = max(ans, val);
     	}
